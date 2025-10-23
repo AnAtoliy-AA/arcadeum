87af18a0106ed37cf977830e3bb4f637ec1c8b5e
AnAtoliy-AA <aliaksandrau89@gmail.com>
2025-10-23 21:49:50 +0400

    fix: settings icon

--- fe/components/ui/IconSymbol.tsx
+++ fe/components/ui/IconSymbol.tsx
@@ -24,6 +24,7 @@ const MAPPING = {
   'person.3.fill': 'groups',
   'clock.fill': 'schedule',
   'sparkles': 'auto-awesome',
+  'gearshape.fill': 'settings',
   'lock.fill': 'lock',
   'lock.open': 'lock-open',
   'book': 'menu-book',

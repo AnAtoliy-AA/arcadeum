e33bba0001107c0af2b47813737adf9dbbe2d59e
AnAtoliy-AA <aliaksandrau89@gmail.com>
2026-03-03 03:33:10 +0400

    fix: return  web unit tests

--- apps/be/test/app.e2e-spec.ts
+++ apps/be/test/app.e2e-spec.ts
@@ -24,6 +24,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toMatchObject({ status: 'ok' });
+      });
   });
 });

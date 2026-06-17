6b7a12ab3e12ca9f3f484e805b5181c60649f42c
AnAtoliy-AA <aliaksandrau89@gmail.com>
2026-06-17 15:54:13 +0400

    feat(solana): add admin buy-back endpoint for token purchases

--- apps/be/src/solana/solana.module.ts
+++ apps/be/src/solana/solana.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { SolanaService } from './solana.service';
 import { SolanaController } from './solana.controller';
 import { WalletModule } from '../wallet/wallet.module';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Module({
   imports: [ConfigModule, WalletModule],
   controllers: [SolanaController],
-  providers: [SolanaService],
+  providers: [SolanaService, RolesGuard],
   exports: [SolanaService],
 })
 export class SolanaModule {}

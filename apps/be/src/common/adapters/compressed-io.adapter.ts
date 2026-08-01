07a0f1a92a99f4ac585ac59bd060b0ad4d883da6
AnAtoliy-AA <aliaksandrau89@gmail.com>
2026-08-01 14:48:57 +0400

    perf(be): implement Redis Pub/Sub adapter support in CompressedIoAdapter

--- apps/be/src/common/adapters/compressed-io.adapter.ts
+++ apps/be/src/common/adapters/compressed-io.adapter.ts
@@ -1,11 +1,26 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import Redis from 'ioredis';
 import type { ServerOptions } from 'socket.io';
+import type { INestApplication } from '@nestjs/common';
 
 const COMPRESSION_THRESHOLD_BYTES = 1024;
 
 export class CompressedIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter> | null = null;
+
+  constructor(app: INestApplication) {
+    super(app);
+    const redisUrl = process.env.REDIS_URL;
+    if (redisUrl) {
+      const pubClient = new Redis(redisUrl);
+      const subClient = pubClient.duplicate();
+      this.adapterConstructor = createAdapter(pubClient, subClient);
+    }
+  }
+
   createIOServer(port: number, options?: ServerOptions): unknown {
-    const ioServer = super.createIOServer(port, {
+    const serverOptions = {
       ...options,
       perMessageDeflate: {
         threshold: COMPRESSION_THRESHOLD_BYTES,
@@ -14,7 +29,12 @@ export class CompressedIoAdapter extends IoAdapter {
         clientNoContextTakeover: true,
         serverNoContextTakeover: true,
       },
-    });
-    return ioServer as unknown;
+    };
+
+    if (this.adapterConstructor) {
+      Object.assign(serverOptions, { adapter: this.adapterConstructor });
+    }
+
+    return super.createIOServer(port, serverOptions) as unknown;
   }
 }

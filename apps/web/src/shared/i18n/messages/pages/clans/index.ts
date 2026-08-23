3039ac4237d139db7edb4fa831195289326a17ef
AnAtoliy-AA <aliaksandrau89@gmail.com>
2026-08-24 01:26:36 +0400

    fix(ARC-891): add missing clans i18n translation files

---
+++ apps/web/src/shared/i18n/messages/pages/clans/index.ts
@@ -0,0 +1,7 @@
+import { clansEn } from './en';
+import { clansEs } from './es';
+import { clansFr } from './fr';
+import { clansRu } from './ru';
+import { clansBy } from './by';
+
+export { clansEn, clansEs, clansFr, clansRu, clansBy };
